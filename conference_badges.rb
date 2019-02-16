612785e57b06464f10d61dc30d1627e288b099ca
Ro Flo <iroflood@msn.com>
2019-02-16 06:52:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,10 @@ def badge_maker(name)
 end
   
   def assign_rooms(speakers)
-    puts "Hello, #{name}! You'll be assigned to room #{room}"
+    puts "Hello, #{name}! You'll be assigned to room #{room}"ef assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
